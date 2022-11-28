04fa11608f5852053c1b1e5228fc943c6aa76d2a
Larry Wong <dev.larry.wong@gmail.com>
2022-11-28 15:18:58 +0000

    completed part 2 by turn static data to dynamic using props

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import './App.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Note from '../components/Note';
+import notes from './notes';
 
 function App() {
+  const notesList = notes.map((note) => {
+    return <Note key={note.id} title={note.title} content={note.content} />;
+  });
+
   return (
     <div>
       <Header />
-      <Note />
+      {notesList}
       <Footer />
     </div>
   );
