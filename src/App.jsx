2eb8b1deaf09f04618d3b9107ea0e41a7db46880
Larry Wong <dev.larry.wong@gmail.com>
2022-11-28 12:22:01 +0000

    completed part 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Note from '../components/Note';
 
 function App() {
   return (
     <div>
-      <h1>testing</h1>
+      <Header />
+      <Note />
+      <Footer />
     </div>
   );
 }
